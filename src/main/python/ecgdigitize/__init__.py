1390388bb72f9ac2492a0a9a278aeda104219062
IA Kadu <ia.kadu.ai@gmail.com>
2025-09-01 15:40:19 -0300

    Documentar módulos auxiliares

--- src/main/python/ecgdigitize/__init__.py
+++ src/main/python/ecgdigitize/__init__.py
@@ -1,8 +1,12 @@
-from .ecgdigitize import \
-    estimateRotationAngle, \
-    SignalDetectionMethod, \
-    SignalExtractionMethod, \
-    digitizeSignal, \
-    GridDetectionMethod, \
-    GridExtractionMethod, \
-    digitizeGrid
+"""Atalhos para as principais funções do pacote ``ecgdigitize``."""
+
+from .ecgdigitize import (
+    estimateRotationAngle,
+    SignalDetectionMethod,
+    SignalExtractionMethod,
+    digitizeSignal,
+    GridDetectionMethod,
+    GridExtractionMethod,
+    digitizeGrid,
+)
+
